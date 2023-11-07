60ae1e35f453d6940e1ae48b160cc718bb9f5f75
Jinsu Choi <135982883+suvven1@users.noreply.github.com>
2023-11-07 17:45:24 +0900

    [Test] AWS EC2 Test

--- web/dna/src/axios.js
+++ web/dna/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "http://ec2-3-34-97-212.ap-northeast-2.compute.amazonaws.com:3333",
 });
 
 export default instance;
