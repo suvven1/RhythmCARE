ca4ba0aa2789ec11f85570792fb565b201237022
최진수 <cjs@choejinsuui-MacBookPro.local>
2024-01-29 09:49:48 +0900

    [fix]server url change

--- web/dna/src/axios.js
+++ web/dna/src/axios.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  // baseURL: "http://3.34.97.212:3333",
-  baseURL: "http://115.95.222.206:80",
-  // baseURL: "http://192.168.70.12:80",
-  // baseURL: "http://192.168.1.152:3333",
+  baseURL: "http://192.168.1.6:80",
 });
 
 export default instance;
